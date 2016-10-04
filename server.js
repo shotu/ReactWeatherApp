92f720cebd34db99b955ebcd8c800fb698ac6aa1
Manish Atri <manish@opinioapp.com>
2016-10-04 13:10:10 +0530

    added http redirection

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.port || 3000;
+
+app.use(function (req,res,next) {
+  console.log(JSON.stringify(req.headers,null,2));
+
+  if (req.headers['x-forward-proto'] === 'http') {
+    next();
+  } else {
+      res.redirect('http://'+req.hostname +req.url)
+  }
+
+})
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port '+PORT);
 });
