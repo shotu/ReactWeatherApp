2030a4538263767feb61925501164a6e96e3bfce
Manish Atri <manish@opinioapp.com>
2016-10-04 13:22:52 +0530

    another bug

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.port || 3000;
+
+const PORT = process.env.PORT || 3000;
 
 
 app.use(function (req, res, next) {
@@ -12,13 +13,11 @@ app.use(function (req, res, next) {
   if ( req.headers['x-forwarded-proto'] === 'http' ) {
     next();
   } else {
-      res.redirect('http://'+req.hostname +req.url)
+      res.redirect('http://'+ req.hostname + req.url)
   }
-
 });
 
 app.use(express.static('public'));
-
 
 app.listen(PORT, function () {
   console.log('Express server is up on port '+PORT);
