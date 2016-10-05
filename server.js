c4df96cbae8faacfc354ac4915b73463dd07b596
Manish Atri <manish@opinioapp.com>
2016-10-05 14:15:57 +0530

    added req.connection.encrypted

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-// app.use( function (req, res, next) {
-//   console.log("++++++++++++++",req.connection.encrypted);
-//   if ( req.connection.encrypted === 'undefined' ) {
-//     res.redirect('http://'+req.hostname +req.url)
-//   } else {
-//       next();
-//   }
-// });
+app.use( function (req, res, next) {
+  console.log("++++++++++++++",req.connection.encrypted);
+  if ( req.connection.encrypted === 'undefined' ) {
+    res.redirect('https://'+req.hostname +req.url)
+  } else {
+      next();
+  }
+});
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
